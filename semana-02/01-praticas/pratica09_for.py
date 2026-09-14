5131c46ed52a99f5060e34ef04abdbca2c653938
Rodrigo de Almeida <ralmes.tech@gmail.com>
2026-09-14 12:08:02 -0300

    feat: adiciona pratica 09 for semana 02

---
+++ semana-02/01-praticas/pratica09_for.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Curso: Python Básico - +IFMG
+# Trilha: Python & Big Data - 160h
+# Semana: 02 - Controle de Fluxo
+# Tipo: Prática
+# Atividade: Prática 09 - Laço for
+# Arquivo: pratica09_for.py
+# Autor: Rodrigo Ralmes
+#
+# Objetivo:
+# Compreender a repetição definida utilizando o laço for.
+#
+# Conteúdos praticados:
+# - for
+# - range
+# - repetição definida
+#
+# Status: A executar
+# =============================================================================
+
+
+# ============================================================
+# PROCESSAMENTO E SAÍDA DE DADOS
+# ============================================================
+
+for numero in range(1, 6):
+    print(f"Número: {numero}")
