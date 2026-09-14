a728f9ea24430b8254c38ff19aaeab4239930bfe
Rodrigo de Almeida <ralmes.tech@gmail.com>
2026-09-14 09:45:30 -0300

    feat: adiciona pratica 01 if simples semana 02

---
+++ semana-02/01-praticas/pratica01_if_simples.py
@@ -0,0 +1,38 @@
+
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Curso: Python Básico - +IFMG
+# Trilha: Python & Big Data - 160h
+# Semana: 02 - Controle de Fluxo
+# Tipo: Prática
+# Atividade: Prática 01 - Estrutura if simples
+# Arquivo: pratica01_if_simples.py
+# Autor: Rodrigo Ralmes
+#
+# Objetivo:
+# Compreender o funcionamento de uma estrutura de decisão simples com if.
+#
+# Conteúdos praticados:
+# - if
+# - operador relacional
+# - entrada e saída de dados
+#
+# Status: A executar
+# =============================================================================
+
+
+# ============================================================
+# ENTRADA DE DADOS
+# ============================================================
+
+idade = int(input("Informe sua idade: "))
+
+
+# ============================================================
+# PROCESSAMENTO E SAÍDA DE DADOS
+# ============================================================
+
+if idade >= 18:
+    print("Você é maior de idade.")
+
+print("Fim do programa.")
