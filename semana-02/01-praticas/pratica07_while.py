2e3ce09aaaef35952177a208d5340fb1e1565148
Rodrigo de Almeida <ralmes.tech@gmail.com>
2026-09-14 11:33:29 -0300

    feat: adiciona pratica 07 while semana 02

---
+++ semana-02/01-praticas/pratica07_while.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Curso: Python Básico - +IFMG
+# Trilha: Python & Big Data - 160h
+# Semana: 02 - Controle de Fluxo
+# Tipo: Prática
+# Atividade: Prática 07 - Laço while
+# Arquivo: pratica07_while.py
+# Autor: Rodrigo Ralmes
+#
+# Objetivo:
+# Compreender a repetição controlada por condição utilizando while.
+#
+# Conteúdos praticados:
+# - while
+# - contador
+# - incremento
+#
+# Status: A executar
+# =============================================================================
+
+
+# ============================================================
+# PROCESSAMENTO E SAÍDA DE DADOS
+# ============================================================
+
+contador = 1
+
+while contador <= 5:
+    print(f"Contagem: {contador}")
+    contador += 1
+
+print("Fim da repetição.")
